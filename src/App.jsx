6707aa26573679a3a0715bdf6b1a5610cd2e0997
Peally <pieclandre@gmail.com>
2023-05-27 22:04:52 +0200

    feat : fetch all data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import useFetch from "./hooks/useFetch";
 
 function App() {
-  return <></>;
+  const [data, loading] = useFetch("./src/data/data.json");
+  console.log(data, loading);
+
+  return (
+    <div>
+      <h1>React Hooks</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
