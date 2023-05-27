762692626f6e7f33890d2ef8e55467f24e2d950c
Peally <pieclandre@gmail.com>
2023-05-27 22:04:43 +0200

    feat : create useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState({ data: [], loading: true });
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData({ data: data, loading: false });
+      })
+      .catch((err) => {
+        setData({ data: [], loading: false });
+        console.log(err);
+      });
+  }, [url]);
+
+  return [data.data, data.loading];
+}
