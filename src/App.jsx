5e88fbf944283560f558205c94464b528eb9df87
Peally <pieclandre@gmail.com>
2023-05-29 20:30:47 +0200

    feat : import header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Error from "./pages/error";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   const [data] = useFetch("./src/data/data.json");
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home data={data} />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
