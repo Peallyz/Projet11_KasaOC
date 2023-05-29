48d51d84d14451c7100eef7ba4363a3104820e39
Peally <pieclandre@gmail.com>
2023-05-29 20:31:43 +0200

    feat : create all footer

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+import Navbar from "./Navigation/Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <NavLink to="/">
+        {" "}
+        <img src="./public/assets/logo_orange.png" alt="Logo de Kasa" />{" "}
+      </NavLink>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
