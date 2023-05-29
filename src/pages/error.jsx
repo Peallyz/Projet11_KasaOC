7983a6b643b35b6f6b798f69ff3489c52bb96eff
Peally <pieclandre@gmail.com>
2023-05-29 23:14:02 +0200

    create : error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,5 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const Error = () => {
-  return <div>Erreur</div>;
+  return (
+    <main className="error__page">
+      <h1>
+        <strong>404</strong>Oups! La page que vous demandez n&apos;existe pas.
+      </h1>
+      <NavLink to="/" className={"current"}>
+        Retourner sur la page d&apos;accueil
+      </NavLink>
+    </main>
+  );
 };
 
 export default Error;
