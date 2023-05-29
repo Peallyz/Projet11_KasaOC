cb5b18c152ddea03894a7b99fd14143fc706a2d1
Peally <pieclandre@gmail.com>
2023-05-29 20:31:38 +0200

    feat : create all footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <NavLink to="/">
+        <img src="./public/assets/logo_white.png " alt="Logo de Kasa" />
+      </NavLink>
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
