7bff55f50c474fe8f01a70f6631546f344ff085a
Peally <pieclandre@gmail.com>
2023-05-30 20:18:17 +0200

    Feat : init props data on Home Page and use types

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,12 @@
-const Home = () => {
+import PropTypes from "prop-types";
+
+const Home = ({ data }) => {
+  Home.propTypes = {
+    data: PropTypes.array.isRequired,
+  };
+
+  console.log(data);
+
   return <div>Acceuil</div>;
 };
 
