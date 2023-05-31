9f5432dfe146c7fae5a70b62df68cfad74766df6
Peally <pieclandre@gmail.com>
2023-05-31 18:42:41 +0200

    feat : DOM elements on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,28 @@
 import PropTypes from "prop-types";
+import Herobanner from "../Components/Herobanner/Herobanner";
+import LodgingCard from "../Components/LodgingCard/LodgingCard";
 
 const Home = ({ data }) => {
   Home.propTypes = {
     data: PropTypes.array.isRequired,
   };
-
   console.log(data);
 
-  return <div>Acceuil</div>;
+  return (
+    <main>
+      <Herobanner img="falaises" title="Chez vous, partout et ailleurs" />
+      <section className="lodgings">
+        {data ? (
+          data.map((lodging) => <LodgingCard data={lodging} key={lodging.id} />)
+        ) : (
+          <h2>
+            Il n&apos;y a pas d&apos;hebergements disponibles, revenez plus tard
+            pour les découvrir
+          </h2>
+        )}
+      </section>
+    </main>
+  );
 };
 
 export default Home;
