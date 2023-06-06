55dc218c0e4c2337f3e5ec4e881d64dd4e5a9029
Peally <pieclandre@gmail.com>
2023-06-06 19:39:48 +0200

    feat : handle loader display with loading from usefetch

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,17 +2,24 @@ import PropTypes from "prop-types";
 import Herobanner from "../Components/Herobanner/Herobanner";
 import LodgingCard from "../Components/Lodging/LodgingCard";
 import Loader from "../Components/Loader/Loader";
+import { useEffect, useState } from "react";
 
-const Home = ({ data }) => {
+const Home = ({ data, loading }) => {
   Home.propTypes = {
-    data: PropTypes.array.isRequired,
+    data: PropTypes.array,
+    loading: PropTypes.bool.isRequired,
   };
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(loading);
+  }, [loading]);
   // display loader while data is not fetched then display the lodgings cards
   return (
     <main>
       <Herobanner img="falaises" title="Chez vous, partout et ailleurs" />
       <section className="lodgings">
-        {data ? (
+        {!isLoading ? (
           data.map((lodging) => <LodgingCard data={lodging} key={lodging.id} />)
         ) : (
           <Loader />
