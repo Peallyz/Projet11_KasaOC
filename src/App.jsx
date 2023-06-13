8c89b1a500591745f51c23515ce422c6ca6b4a4d
Peally <pieclandre@gmail.com>
2023-06-13 20:06:09 +0200

    fix : isolate Router from API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import useFetch from "./hooks/useFetch";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Error from "./Pages/Error";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Lodging from "./pages/Lodging";
-import { Navigate } from "react-router-dom";
+import Router from "./Router";
 
 function App() {
-  // Fetch the data from the json file
-  const [data, loading] = useFetch("./src/data/data.json");
-
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home data={data} loading={loading} />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/:id"
-          element={<Lodging data={data} loading={loading} />}
-        />
-        <Route path="/error" element={<Error />} />
-        <Route path="*" element={<Navigate to="/error" />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <Router />;
 }
 
 export default App;
