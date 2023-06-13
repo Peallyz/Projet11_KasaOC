9471426f21aa9ac23f2aefa138b642a5a50ece90
Peally <pieclandre@gmail.com>
2023-06-13 20:23:14 +0200

    refacto : accordion can now handle any DOM elements

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,39 +1,11 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-const Accordion = ({ type, title = "", data, isDynamic }) => {
+const Accordion = ({ title = "", dataComponent }) => {
   // Handle the accordion opening and closing state to display the content and turn the chevron
   const [isClosed, setIsClosed] = useState(true);
 
-  return isDynamic ? (
-    data ? (
-      <div className={isClosed ? "accordion close" : "accordion"}>
-        <div
-          className="accordion__title"
-          onClick={() => setIsClosed(!isClosed)}
-        >
-          <h3>{type === "description" ? "Description" : "Equipements"}</h3>
-          <img
-            src="./assets/chevron.svg"
-            alt="chevron pour dérouler les informations textuelles"
-          />
-        </div>
-        <div className="accordion__content">
-          {type === "description" ? (
-            <p>{data.description}</p>
-          ) : (
-            <ul>
-              {data.equipments.map((equipment) => (
-                <li key={equipment}>{equipment}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-    ) : (
-      ""
-    )
-  ) : (
+  return dataComponent ? (
     <div className={isClosed ? "accordion close" : "accordion"}>
       <div className="accordion__title" onClick={() => setIsClosed(!isClosed)}>
         <h3>{title}</h3>
@@ -42,17 +14,15 @@ const Accordion = ({ type, title = "", data, isDynamic }) => {
           alt="chevron pour dérouler les informations textuelles"
         />
       </div>
-      <div className="accordion__content">
-        <p>{data.description}</p>
-      </div>
+      <div className="accordion__content">{dataComponent}</div>
     </div>
+  ) : (
+    ""
   );
 };
 Accordion.propTypes = {
-  type: PropTypes.string.isRequired,
   title: PropTypes.string,
-  data: PropTypes.object,
-  isDynamic: PropTypes.bool.isRequired,
+  dataComponent: PropTypes.object,
 };
 
 export default Accordion;
